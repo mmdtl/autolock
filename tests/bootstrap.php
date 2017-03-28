65543aa364bc0927d81ead5feb7f2aec5a7a27fc
liulu <liu.lu@xiji.com>
2017-03-28 13:29:11 +0800

    edit config for auto build

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 
 $loader = @include __DIR__ . '/../vendor/autoload.php';
-if (!$loader) {
-    $loader = require __DIR__ . '/../../../../vendor/autoload.php';
-}
-$loader->addPsr4('AutoLock\\Test\\', __DIR__);
